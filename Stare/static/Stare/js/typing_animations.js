8194e8cc8ca4c68a16eaf4f73203516444d479a3
Deepak Bharti <deepakbharti823@gmail.com>
2018-03-18 08:43:53 +0530

    awesome

---
+++ Stare/static/Stare/js/typing_animations.js
@@ -0,0 +1,105 @@
+
+
+$(document).ready(function(){
+
+$('.stare_text').typeIt({ 
+  // breakLines:false, 
+  speed: 100,
+  // deleteDelay: 750, 
+  autoStart: false,
+  loop:true,
+  // loopDelay:1500 
+});
+
+$('.who_text').typeIt({ 
+  breakLines:false, 
+  speed: 300,
+  // deleteDelay: 750, 
+  autoStart: false,
+  loop:true,
+  loopDelay:1500 
+});
+
+$('.type_personal,.type_parental,.type_future,.type_untold').typeIt({ 
+  // breakLines:false, 
+  speed: 100,
+  // deleteDelay: 750, 
+  autoStart: false,
+  loop:false,
+  // loopDelay:1500 
+});
+
+// $('.type_slider').typeIt({    
+//   speed: 100,
+//   autoStart: false,
+// });
+
+// $('.type_why_woc').typeIt({  
+//   speed: 100,
+//   autoStart: false,
+// });
+
+// $('.type_why_woc_after').typeIt({  
+//   speed: 200,
+//   startDelay:9000,
+//   autoStart: false,
+// });
+
+// $('.type_sponsor').typeIt({  
+//   speed: 250,
+//   loop:true,
+//   autoStart: false,
+// });
+
+// $('.type_woc10').typeIt({  
+//   speed: 300,
+//   loop:true,
+//   autoStart: false,
+// });
+
+// $('.type_devjam_codestart').typeIt({  
+//   speed: 100,
+//   autoStart: false,
+// });
+
+
+// $('.type_event_location').typeIt({  
+//   speed: 100,
+//   // startDelay: 100,
+//   autoStart: false,
+// });
+
+// $('.type_event_time').typeIt({  
+//   speed: 100,
+//   startDelay: 2500,
+//   autoStart: false,
+// });
+
+// $('.type_event_speakers').typeIt({  
+//   speed: 100,
+//   startDelay: 4500,
+//   autoStart: false,
+// });
+
+// $('.type_event_registration').typeIt({  
+//   speed: 100,
+//   startDelay: 6500,
+//   autoStart: false,
+// });
+
+// $('.type_devjam').typeIt({  
+//   speed: 100,
+//   loop:true,
+//   autoStart: false,
+// });
+// </script>
+
+// <script type="text/javascript">
+// $('.type_codestart').typeIt({  
+//   speed: 100,
+//   loop:true,
+//   autoStart: false,
+// });
+
+});
+
